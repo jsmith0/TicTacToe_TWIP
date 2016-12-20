42304011da9266aa3e67c7f77e9229251ed94d7d
jsmith0 <justins4@comcast.net>
2016-12-19 22:30:30 -0500

    asdf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,42 +19,52 @@ public class Main {
         String i = "9";
         String board = "\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n";
         System.out.println(board);
-        System.out.println("Now, choose your spot. Or type \"0\" if you want to go second");
+        System.out.println("Now make your move. Enter the number of the position you want. Or ype \"0\" to go second");
         Random rd = new Random();
         String input = kb.next();
-        while(true) {
-            if (input.equals(0)) {
-                System.out.println("You will go second");
-            }
-            else if (input.equals(1)) {
-                a = "X";
-            }
-            System.out.println(board);
-            else if (input.equals(2))
-                b = "X";
-            System.out.println(board);
-            else if (input.equals(3)) {
-                c = "X";
-                System.out.println(board);
-            }
-            if (input.equals(4))
-                d = "X";
-            System.out.println(board);
-            if (input.equals(5))
-                e = "X";
-            System.out.println(board);
-            if (input.equals(6))
-                f = "X";
-            System.out.println(board);
-            if (input.equals(7))
-                g = "X";
-            System.out.println(board);
-            if (input.equals(8))
-                h = "X";
-            System.out.println(board);
-            if (input.equals(9))
-                i = "X";
-            System.out.println(board);
+
+        if (input.equals("0")) {
+            System.out.println("You will go second");
         }
+        else if (input.equals("1")) {
+            a = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+        else if (input.equals("2")) {
+            b = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+        else if (input.equals("3")) {
+            c = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+        else if (input.equals("4")) {
+            d = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+        else if (input.equals("5")) {
+            e = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+        else if (input.equals("6")) {
+            f = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+        else if (input.equals("7")) {
+            g = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+        else if (input.equals("8")) {
+            h = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+        else if (input.equals("9")) {
+            i = "X";
+            System.out.println("\n "+a+" | "+b+" | "+c+"\n---|---|--- \n "+d+" | "+e+" | "+f+"\n---|---|--- \n "+g+" | "+h+" | "+i+"\n");
+        }
+
+        int Os = 1 + rd.nextInt(8);
+
+
     }
 }
